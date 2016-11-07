93d3ca3ca103bc2e81fc407791388a42d6445967
Diego García <xaethos@gmail.com>
2016-11-08 01:16:07 +0800

    Hello world

---
+++ app/app.component.js
@@ -0,0 +1,11 @@
+(function(app) {
+  app.AppComponent =
+    ng.core.Component({
+      selector: 'my-app',
+      template: '<h1>Hello World!</h1>'
+    })
+    .Class({
+      constructor: function() {}
+    });
+})(window.app || (window.app = {}));
+
